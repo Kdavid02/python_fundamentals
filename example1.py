108f00d7fba6378e8d0e9efe4727e57b7e6184ca
kdavid <kostnerdavid02@gmail.com>
2022-03-10 15:16:37 -0600

    Added excerise 8-1 and 8-2

--- example1.py
+++ example1.py
@@ -1,1 +1,9 @@
-print("Hello World")
+def display_message():
+    print("I'm learning how to write functions with arguments and parameters in python")
+display_message()
+
+
+def favorite_book():
+    print("One of my favorite books is To Kill A Mockingbird")
+
+favorite_book()
