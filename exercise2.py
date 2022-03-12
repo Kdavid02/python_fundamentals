f57fff6dff933778ada74804560ed7c99dfca090
kdavid <kostnerdavid02@gmail.com>
2022-03-12 08:44:19 -0600

    Added exercise2.py

---
+++ exercise2.py
@@ -0,0 +1,37 @@
+def simple():
+    message1 = "This is message 1"
+    message2 = "This is message 2"
+    message3 = "This is message 3"
+    message4 = "This is message 4"
+    message5 = "This is message 5"
+    print(message1)
+    print(message2)
+    print(message3)
+    print(message4)
+    print(message5)
+
+
+simple()
+
+
+def simple2():
+    message1 = "This is message 1"
+    print(message1)
+    message1 = "This is message 2"
+    print(message1)
+    message1 = "This is message 3"
+    print(message1)
+    message1 = "This is message 4"
+    print(message1)
+
+
+simple2()
+
+
+def message(msg1):
+    print(msg1)
+
+
+message(msg1="This is a message")
+
+
