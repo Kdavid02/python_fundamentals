78d3167944f53262041177ad8c98d92740bbef4f
kdavid <kdavid@wsutech.edu>
2022-03-13 12:15:22 -0500

    CAC-102 Added exercise3.py

---
+++ lessons/exercise3.py
@@ -0,0 +1,32 @@
+# 2.3
+
+name = "Nate"
+
+print(f"It is {name}'s birthday!")
+# 2.4
+
+name2 = "Matt"
+
+print(name2.capitalize())
+print(name2.lower())
+print(name2.title())
+# 2.5
+
+quote = "If you can't explain it simply, you don't understand it well enough."
+print(f"Albert Einstein said \"{quote}\"")
+
+# 2.6
+
+quote = "If you can't explain it simply, you don't understand it well enough."
+famous_person = "Albert Einstein"
+message = famous_person + " said " + quote
+print(message)
+
+# 2.7
+
+name3 = " \t Albert \n Einstein"
+print(name3)
+print(name3.lstrip())
+print(name3.rstrip())
+print(name3.strip())
+
