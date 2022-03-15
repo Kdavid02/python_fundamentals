a427db0632515c294461f9825fddc7d4af036bc7
kdavid <kdavid@wsutech.edu>
2022-03-15 11:03:01 -0500

    CAC-102-5 Operators

---
+++ lessons/exercise5.py
@@ -0,0 +1,85 @@
+
+color = 'red'
+print('Is color == red? I predict True')
+print(color == 'red')
+
+print("\nIs color == number? I predict False")
+print(color == 'number')
+
+pancake = 'food'
+print("\nIs pancake == food? I predict True")
+print(pancake == 'food')
+
+print("\nIs pancake == Toy? I predict False")
+print(pancake == 'Toy')
+
+happy = 'emotion'
+print("\nIs happy == 'emotion' ? I predict True")
+print(happy == 'emotion')
+
+print("\nIs happy = food ? I predict False")
+print(happy == 'food')
+
+snake = 'animal'
+print("\nIs snake == animal ? I predict True")
+print(snake == 'animal')
+
+print("\nIs snake == mammal ? I predict False")
+print(snake == 'mammal')
+
+frog = 'amphibians'
+print("\nIs frog == amphibians? I predict True")
+
+frog = 'rock'
+print("\nIs frog == rock ? I predict False")
+
+calculator = 'device'
+print("\nIs calculator == device ? I predict True ")
+print(calculator == 'device')
+print("\nIs calculator == doctor ? I predict False")
+print(calculator == 'doctor')
+
+print("------------")
+
+
+# 5-2
+
+print("red = red?")
+print('red' == 'red')
+
+print("blue = red?")
+print('blue' == 'red')
+
+print('is brand = nike. True')
+brand = 'Nike'
+print(brand.lower() == 'nike')
+print('is brand = toad False')
+brand.lower = 'toad'
+
+print('3 is equal to 3. True?')
+print(3 == 3)
+print('3 is not equal to 3. False?')
+print(3 != 3)
+
+print('3 greater than or equal to 3. True?')
+print(3 >= 3)
+print('3 less than or equal to 2. False?')
+print(3 <= 2)
+
+print('3 less than 2? True?')
+print(3 > 2)
+print('3 less than 2? False?')
+print(3 < 2)
+
+print('3 equals 3. and 3 less than 2. False')
+print(3 == 3 and 3 < 2)
+print('9 equals 9 or 5 greater than 4. True')
+print(9 == 9 or 5 > 4)
+
+animals = ['toad', 'cat', 'dog']
+print('is cat animal? True?')
+
+print('cat' in animals)
+
+print('is car animal? False?')
+print('car' in animals)
